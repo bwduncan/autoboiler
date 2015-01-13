0a1e79e96ad14d2895e68f3ce4ba9669381a0d82
Bruce Duncan <bwduncan@gmail.com>
2015-01-13 16:55:30 +0000

    Change channel to 96 to avoid Wifi interference.

--- autoboiler.py
+++ autoboiler.py
@@ -61,6 +61,7 @@ class Boiler:
         self.temperature = temperature
         self.radio = nrf24.NRF24()
         self.radio.begin(major, minor, ce_pin, irq_pin)
+        self.radio.setChannel(96)
         self.radio.enableDynamicPayloads()
         self.radio.printDetails()
         self.radio.openWritingPipe(PIPES[0])
@@ -109,6 +110,7 @@ class Controller:
         self.sock = sock
         self.radio = nrf24.NRF24()
         self.radio.begin(major, minor, ce_pin, irq_pin)
+        self.radio.setChannel(96)
         self.radio.enableDynamicPayloads()
         self.radio.printDetails()
         self.radio.openWritingPipe(PIPES[0])
